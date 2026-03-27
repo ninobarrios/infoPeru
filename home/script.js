5a53b7a301c279be94e33cc4063d488fa72a9eff
nino <nicolas6x@hotmail.com>
2026-03-26 22:05:34 -0500

    version final 3.0 corregido version full

--- home/script.js
+++
@@ -1,9 +0,0 @@
-// faq.js
-document.addEventListener('DOMContentLoaded', () => {
-    document.querySelectorAll(".faq-question").forEach(button => {
-        button.addEventListener("click", () => {
-            const item = button.parentElement;
-            item.classList.toggle("active");
-        });
-    });
-});
